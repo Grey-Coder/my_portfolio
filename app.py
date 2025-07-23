0d877a21c7a7479e1bc228f1329ffccdbc8c8b2b
AMIT ACHARJEE <117907179+Grey-Coder@users.noreply.github.com>
2025-07-24 02:09:10 +0530

    new timezone update

--- app.py
+++ app.py
@@ -2,22 +2,21 @@ from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import os
+from pytz import timezone  # <-- ADD THIS IMPORT
 
 app = Flask(__name__, static_url_path='', static_folder='.')
 
 # --- Database Setup ---
-# Get the database URL from the environment variable set in Render
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-# Define the Submission database model
 class Submission(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), nullable=False)
     message = db.Column(db.Text, nullable=False)
-    timestamp = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    timestamp = db.Column(db.DateTime, nullable=False) # We will set the default in the route
 
     def __repr__(self):
         return f'<Submission {self.name}>'
@@ -25,7 +24,6 @@ class Submission(db.Model):
 
 @app.route('/')
 def home():
-    # Create the database tables if they don't exist
     with app.app_context():
         db.create_all()
     return render_template('index.html')
@@ -36,12 +34,16 @@ def submit_form():
         name = request.form['name']
         email = request.form['email']
         message = request.form['message']
-
+        
+        # --- CHANGE THIS LINE ---
+        # Get the current time in the 'Asia/Kolkata' timezone
+        india_time = datetime.now(timezone('Asia/Kolkata'))
+        
         # Create a new submission object and add it to the database
-        new_submission = Submission(name=name, email=email, message=message)
+        new_submission = Submission(name=name, email=email, message=message, timestamp=india_time)
         db.session.add(new_submission)
         db.session.commit()
-
+        
         return """
         <html>
             <head><title>Thank You</title></head>
@@ -52,6 +54,6 @@ def submit_form():
             </body>
         </html>
         """
-
+        
 if __name__ == '__main__':
     app.run()
